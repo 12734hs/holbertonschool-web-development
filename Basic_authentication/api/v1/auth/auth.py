bae6c38dc92db98a53eca2f9c35a802ee148e184
Sabir <12734@holbertonstudents.com>
2026-04-20 12:11:20 +0400

    Fixed some stuff

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,12 +7,15 @@ from typing import List, TypeVar
 class Auth:
     """We gonna use that class for auth goals"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """This funct is require the auth"""
         return False
 
 
     def authorization_header(self, request=None) -> str:
+        """This funct is auto header"""
         return None
 
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """This funct is checking current user"""
         return None
