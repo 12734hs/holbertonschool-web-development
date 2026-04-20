911b59a5d180d53018f5d91f13c5984e0ffabce7
Sabir <12734@holbertonstudents.com>
2026-04-20 13:20:28 +0400

    Stuff for the task 7, style

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,9 +5,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """This is basic auth documented class baby"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """We will use this method for taking basic from the header"""
-        if authorization_header is None or type(authorization_header) is not str:
+        if (authorization_header is None or
+                type(authorization_header) is not str):
             return None
         if authorization_header[:6] != "Basic ":
             return None
