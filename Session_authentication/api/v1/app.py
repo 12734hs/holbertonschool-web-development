7a6dc8229a2252f5827a796748a7de383898d5f3
Sabir <12734@holbertonstudents.com>
2026-04-21 14:58:53 +0400

    Stuff for the task 0, session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -49,16 +49,17 @@ def forbidden(error) -> tuple:
 def before_request():
     """Special checker which check the situation for our user"""
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    user = auth.current_user(request)
     if auth is None:
         return
     if not auth.require_auth(request.path, paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+    if user is None:
         abort(403)
 
-    request.current_user = auth.current_user(request)
+    request.current_user = user
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
