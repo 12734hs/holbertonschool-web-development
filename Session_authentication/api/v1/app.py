c55e19ce42f093f631ff85127abd521b0acc457e
Sabir <12734@holbertonstudents.com>
2026-04-22 11:11:38 +0400

    Stuff for the task 5, session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -55,9 +55,8 @@ def before_request():
         return
     if not auth.require_auth(request.path, paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.session_cookie(request) is None:
+    if (auth.authorization_header(request) is None or
+            auth.session_cookie(request) is None):
         abort(401)
 
     request.current_user = auth.current_user(request)
