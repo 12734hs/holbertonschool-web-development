859660d281db321ab5b96e23bd2344ecd65a48e8
Sabir <12734@holbertonstudents.com>
2026-04-22 11:00:34 +0400

    Stuff for the task 2, session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,10 @@ class SessionAuth(Auth):
         session_code = uuid4()
         self.user_id_by_session_id[session_code] = user_id
         return session_code
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """We us ethis method for find user by his session"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
