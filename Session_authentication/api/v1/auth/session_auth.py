7acef8e0ce684bc14117708958a05976b7123fe4
Sabir <12734@holbertonstudents.com>
2026-04-22 10:58:54 +0400

    Stuff for the task 2, session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 class SessionAuth(Auth):
     """This class is about session mechanism in flask"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """We use that method for summon the session for user"""
         if user_id is None or not isinstance(user_id, str):
