f324a18b78b35c39477819b34d38519536c4c94f
Sabir <12734@holbertonstudents.com>
2026-04-30 21:23:37 +0400

    UAS

---
+++ user_authentication_service/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+We created that file for create the function for pagination
+"""
+
+
+def index_range(page=1, page_size=10):
+    """
+    We use that function for creating pagination main variables
+    start we use for found out when the data starts
+    end for find out when we have to stop show our data
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
