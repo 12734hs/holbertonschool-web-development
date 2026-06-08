85a21d7b761002c642785dad0a5be34d4b65c5fa
Sabir <12734@holbertonstudents.com>
2026-06-08 16:54:42 +0400

    UAS, task 3

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+"""This file is about auth in our service"""
+import bcrypt
+
+def _hash_password(password) -> str:
+    """method which hashes and convert it to bytes"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
