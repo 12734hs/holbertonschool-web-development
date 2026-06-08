2da07699dbd6f613ace31282fbab2936d820421f
Sabir <12734@holbertonstudents.com>
2026-06-08 17:32:38 +0400

    UAS, task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -32,7 +32,8 @@ class Auth:
         """Create method for valid login"""
         try:
             user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8')):
+            if bcrypt.checkpw(password.encode('utf-8'),
+                              user.hashed_password.encode('utf-8')):
                 return True
         except (NoResultFound, AttributeError):
             return False
