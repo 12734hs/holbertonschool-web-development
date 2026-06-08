0d1db1b546086b695aff8d1a7cad4c73612e2741
Sabir <12734@holbertonstudents.com>
2026-06-08 19:41:58 +0400

    UAS, task 14

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,3 +76,12 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email):
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
