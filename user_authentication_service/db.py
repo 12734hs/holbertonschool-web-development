06344d058fec04c13ca743669830e27643deb444
Sabir <12734@holbertonstudents.com>
2026-06-08 16:50:40 +0400

    UAS, task 3

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -47,7 +47,7 @@ class DB:
         """THis method updates the user info"""
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if hasattr(user, key):
+            if not hasattr(user, key):
                 raise ValueError
             setattr(user, key, value)
 
