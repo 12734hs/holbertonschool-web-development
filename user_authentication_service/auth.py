6d9e1542209440b7e3b8778b20d4f39b666a351b
Sabir <12734@holbertonstudents.com>
2026-06-08 19:26:01 +0400

    UAS, task 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -71,7 +71,7 @@ class Auth:
     def destroy_session(self, user_id):
         """This method destroys session id"""
         try:
-            user = self._db.find_user_by(user_id=user_id)
+            user = self._db.find_user_by(id=user_id)
             self._db.update_user(user_id, session_id=None)
             return None
         except NoResultFound:
