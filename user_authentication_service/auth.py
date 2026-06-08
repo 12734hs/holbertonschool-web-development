889405cea1716ff66edfccdece557c9adacbeb4b
Sabir <12734@holbertonstudents.com>
2026-06-08 17:12:09 +0400

    UAS, task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -25,6 +25,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
-                hash_pwd = _hash_password(password)
-
-        return self._db.add_user(email, hash_pwd)
+            hash_pwd = _hash_password(password)
+            return self._db.add_user(email, hash_pwd)
