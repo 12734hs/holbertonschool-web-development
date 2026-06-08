c580b44a696cd2a14610e41ee8a7870fef3caa8d
Sabir <12734@holbertonstudents.com>
2026-06-08 19:49:40 +0400

    UAS, task 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,15 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token, password):
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        new_psw = _hash_password(password)
+        self._db.update_user(user.id, password=new_psw)
+        self._db.update_user(user.id, reset_token=None)
+
+        return None
