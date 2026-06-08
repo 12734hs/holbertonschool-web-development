0d7f65f2c574d601476bffe10d445fe226e9a328
Sabir <12734@holbertonstudents.com>
2026-06-08 19:17:18 +0400

    UAS, task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -23,6 +23,7 @@ def users():
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
 
+
 @app.route('/sessions', methods=['POST'])
 def session():
     email = request.form.get('email')
