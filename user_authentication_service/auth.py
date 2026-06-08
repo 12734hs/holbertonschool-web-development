d1c709b8acaffea26665a961eacc5754a3b6d72f
Sabir <12734@holbertonstudents.com>
2026-06-08 17:06:48 +0400

    UAS, task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """This file is about auth in our service"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +10,21 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """We use that method for creating user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except:
+                hash_pwd = _hash_password(password)
+
+        return self._db.add_user(email, hash_pwd)
