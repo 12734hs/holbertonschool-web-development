d3a2260385737355a99667b438668825e8c70c3d
Sabir <12734@holbertonstudents.com>
2026-06-08 19:51:52 +0400

    UAS, task 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -93,7 +93,6 @@ class Auth:
             raise ValueError
 
         new_psw = _hash_password(password)
-        self._db.update_user(user.id, password=new_psw)
-        self._db.update_user(user.id, reset_token=None)
+        self._db.update_user(user.id, hashed_password=new_psw, reset_token=None)
 
         return None
