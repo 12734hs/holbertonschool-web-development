8f9afd96c00ae4ad538cb4cdb74a9693870953b9
Sabir <12734@holbertonstudents.com>
2026-06-08 17:21:59 +0400

    UAS, task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 """THis file is the basic file of our project"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({'email':f'{email}', 'message': 'user created'})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":
