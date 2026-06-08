506238708906ae65634db2061f54f46fd7b96639
Sabir <12734@holbertonstudents.com>
2026-06-08 19:35:42 +0400

    UAS, task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -48,5 +48,14 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile')
+def profile():
+    ss_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(ss_id)
+    if user is None:
+        abort(403)
+    return jsonify({'email': f'{user.email}'}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
