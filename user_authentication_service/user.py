cefb3bed11ffc59fcc2c819c7da03fe99a49ef57
Sabir <12734@holbertonstudents.com>
2026-06-08 16:06:42 +0400

    UAS, task 0

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 base = declarative_base()
 
+
 class User(base):
     """This class is about thebase user class in DB"""
     __tablename__ = 'users'
